a9e0ae829c4b62ee7116e6f22c7b99aa789f8a56
AllanDC1 <adonetticalen@gmail.com>
2025-04-29 18:12:12 -0300

    Classe Conexao JDBC criada

---
+++ src/main/java/spotifei/util/Conexao.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spotifei.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author adone
+ */
+public class Conexao {
+        
+    private static final String USUARIO = "";
+    private static final String SENHA = "";
+    private static final String URL_BD = "";
+    
+    public Connection criarConexaoBD() throws SQLException {
+        Connection conexao = DriverManager.getConnection(URL_BD, USUARIO, SENHA);
+        return conexao;
+    } 
+    
+}
