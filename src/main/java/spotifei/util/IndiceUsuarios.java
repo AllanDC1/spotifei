019a6a96f7aad43999b76d4e18f458b77e66f48e
AllanDC1 <adonetticalen@gmail.com>
2025-05-03 19:59:05 -0300

    feat: Classe para legibilidade dos índices da tabela usuarios

---
+++ src/main/java/spotifei/util/IndiceUsuarios.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spotifei.util;
+
+/**
+ *
+ * @author adone
+ */
+public class IndiceUsuarios {
+    public static final int ID_USUARIO = 1;
+    public static final int LOGIN_USUARIO = 2;
+    public static final int SENHA_USUARIO = 3;
+    public static final int NOME_USUARIO = 4;
+}
