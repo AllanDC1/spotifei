e0f983c1c9cb5d3e7ffb074eabbe90af8ccf1b9c
AllanDC1 <adonetticalen@gmail.com>
2025-05-03 11:32:58 -0300

    feat: Classe para legibilidade de Erros SQL

---
+++ src/main/java/spotifei/util/ErroSQL.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package spotifei.util;
+
+/**
+ *
+ * @author adone
+ */
+public class ErroSQL {
+    public static final String ERRO_UNIQUE_POSTGRESQL = "23505";
+}
