66e0b14305c52c4349105862cc9cd4d1ab9e2dae
AllanDC1 <adonetticalen@gmail.com>
2025-05-18 21:42:20 -0300

    refactor: Alterado a forma de conexão utilizando .properties no diretório resources

--- src/main/java/spotifei/dao/Conexao.java
+++ src/main/java/spotifei/dao/Conexao.java
@@ -4,22 +4,45 @@
  */
 package spotifei.dao;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+import javax.swing.JOptionPane;
 
 /**
+ * Classe responsável por fornecer conexões com o banco de dados PostgreSQL.
  *
- * @author adone
+ * <p>Utiliza parâmetros fixos para usuário, senha e URL de conexão com o banco.</p>
  */
 public class Conexao {
         
-    private static final String USUARIO = "postgres";
-    private static final String SENHA = "admin";
-    private static final String URL_BD = "jdbc:postgresql://localhost:5432/spotifei";
+    private static final Properties propriedades = new Properties();
     
+    static {
+        try (InputStream input = Conexao.class.getClassLoader().getResourceAsStream("bd.properties")) {
+            if (input == null) {
+                JOptionPane.showMessageDialog(null, "Arquivo de configuração 'bd.properties' não encontrado: ", "Erro", JOptionPane.ERROR_MESSAGE);
+            }
+            propriedades.load(input);
+        } catch (IOException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao carregar o arquivo de configuração: " + e.getMessage(), "Erro", JOptionPane.ERROR_MESSAGE);
+        }
+    }    
+    /**
+     * Cria e retorna uma nova conexão com o banco de dados usando os parâmetros do arquivo bd.properties.
+     *
+     * @return conexão com o banco de dados
+     * @throws SQLException se houver erro na conexão
+     */
     public static Connection criarConexaoBD() throws SQLException {
-        Connection conexao = DriverManager.getConnection(URL_BD, USUARIO, SENHA);
+        String usuario = propriedades.getProperty("bd.usuario");
+        String senha = propriedades.getProperty("bd.senha");
+        String url = propriedades.getProperty("bd.url");
+        
+        Connection conexao = DriverManager.getConnection(url, usuario, senha);
         return conexao;
     } 
 }
